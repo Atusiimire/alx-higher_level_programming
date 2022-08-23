50a074782b9682d9acd7cbc3ac4129e77f80049c
Atusiimire <mutatinamoureen@gmail.com>
2022-08-23 13:45:55 -0700

    update

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def to_uppa(chr):
-if ord(chr) >= 97 and ord(chr) <= 122:
-return (ord(chr)-32)
-else:
-return ord(chr)
+    if ord(chr) >= 97 and ord(chr) <= 122:
+        return (ord(chr)-32)
+    else:
+        return ord(chr)
 def uppercase(str):
 new_str = ""
-for chr in str:
-new_str += "%c" % to_uppa(chr)
-print("{:s}".format(new_str))
+    for chr in str:
+        new_str += "%c" % to_uppa(chr)
+        print("{:s}".format(new_str))
