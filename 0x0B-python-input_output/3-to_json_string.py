4758975508d095fa562f957109a220cd916a3c50
Atusiimire <mutatinamoureen@gmail.com>
2022-09-27 08:38:09 -0700

    first commit

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""
-Contains the function "wrtie_file"
-"""
+'''to_json_string module definition'''
+
+import json
+'''importing json to module'''
 
 
-def write_file(filename="", text=""):
-    """returns the number of chars written to "filename" from "text" """
-    with open(filename, 'w', encoding='utf=8') as f:
-        return f.write(text)
+def to_json_string(my_obj):
+    '''to_json_string - this function uses json to convert object to str
+    Args:
+        my_obj: this is the object to be converted
+        Returns: it returns the new string
+    '''
+    return json.dumps(my_obj)
