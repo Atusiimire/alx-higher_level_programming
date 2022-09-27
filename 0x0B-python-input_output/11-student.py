2c84ad0bc91b2d112ae4020f2da4ec611021b86a
Atusiimire <mutatinamoureen@gmail.com>
2022-09-27 14:20:50 -0700

    first commit

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-"""
-Contains the clas "Student"
-"""
+"""Contains a Student class"""
 
 
 class Student:
-    """Representation of a student"""
+    """defines a student object"""
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-    """returns a dictionary representation of a Student instance"""
-    return self.__dict__
+    def to_json(self, attrs=None):
+    """retrieves a dictionary representation of a Student instance"""
+    if type(attrs) is list and all(type(s) is str for s in attrs):
+        res = {}
+        for key in attrs:
+            if key in self.__dict__:
+                res[key] = self.__dict__[key]
+        return res
+    else:
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for key in json:
+            if key in self.__dict__:
+                self.__dict__[key] = json[key]
--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -9,3 +9,13 @@ def pascal_triangle(n):
 
     if n <= 0:
         return []
+
+    for i in range(n):
+        if len(result) in [0, 1]:
+            result.append([1] * (len(result) + 1))
+            continue
+        temp = [1, 1]
+        for i in range(len(result[-1]) - 1):
+            temp.insert(i+1, result[-1][i] + result[-1][i+1])
+        result.append(temp)
+    return result
