b436f6c9aa5e9b4324074678efa8d7ec828a842a
Atusiimire <mutatinamoureen@gmail.com>
2022-11-01 05:36:49 -0700

    first commit

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,5 +1,2 @@
--- script to display score and the number of times it occurs
-SELECT score, COUNT(score) AS number 
-FROM second_table 
-GROUP BY score
-ORDER BY score DESC;
+-- get the average of scores in a table
+SELECT AVG(score) AS average FROM second_table;
