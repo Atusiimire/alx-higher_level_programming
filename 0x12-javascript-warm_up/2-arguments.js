865ac057ce6a5e828ed3ac978ba1dda8011cd4ec
Atusiimire <mutatinamoureen@gmail.com>
2022-11-28 12:41:06 -0800

    first commit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // A script that prints a message depending on Args
 if (process.argv[3]) {
-	console.log('Arguments found');
+  console.log('Arguments found');
 } else if (process.argv[2]) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('No argument');
+  console.log('No argument');
 }
