14fc215bf83c8dbfec60050a1e6e262e463d60c2
Atusiimire <mutatinamoureen@gmail.com>
2022-11-29 12:32:53 -0800

    first commit

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-let count = 0;
-list.forEach(function (item) {
-if (item === searchElement) {
-count++;
-}
-});
-return count;
+  let count = 0;
+  list.forEach(function (item) {
+    if (item === searchElement) {
+      count++;
+    }
+  });
+  return count;
 };
