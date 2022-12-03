486501c5800a89fac0fc5113826b1f4c4b812cbc
Atusiimire <mutatinamoureen@gmail.com>
2022-12-03 08:58:25 -0800

    first commit

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
 """
-This module runs a script to fetch all cities table
-in the database provided along with the state name
-equal to the state provided
+return cities that are in the state given (tables 'cities' 'states)
+parameters given to script: username, password, database, state
 """
+
 import MySQLdb
 from sys import argv
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    # state = cur.execute("SELECT id FROM states WHERE name = %s", (argv[4],))
-    # state = cur.fetchone()
-    # cur.execute("SELECT name FROM cities WHERE state_id = %s", state)
-    cur.execute("SELECT c.name from cities c JOIN states s on \
-                c.state_id = s.id WHERE s.name = %s", (argv[4],))
-    cities = cur.fetchall()
-        print(", ".join(city[0] for city in cities))
-    cur.close()
+
+    # connect to database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    # create cursor to exec queries using SQL; join two tables for all info
+    cursor = db.cursor()
+    sql_cmd = """SELECT cities.name
+                 FROM states
+                 INNER JOIN cities ON states.id = cities.state_id
+                 WHERE states.name LIKE %s
+                 ORDER BY cities.id ASC"""
+    cursor.execute(sql_cmd, (argv[4], ))
+
+    # format the printing of cities of same state separated by commas
+    print(', '.join(["{:s}".format(row[0]) for row in cursor.fetchall()]))
+
+    cursor.close()
     db.close()
