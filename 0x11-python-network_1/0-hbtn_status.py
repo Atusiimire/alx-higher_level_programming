08ea178e9386a61edfb716fed158d4bcc0acf3ef
Atusiimire <mutatinamoureen@gmail.com>
2022-12-16 14:24:18 -0800

    first commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""fetch url with urlib"""
-from urllib.request import urlopen
+"""my body header"""
+if __name__ == "__main__":
+    import urllib.request
 
-
-def status():
-    """fetch url body"""
     with urlopen('https://alx-intranet.hbtn.io/status') as response:
-        body = response.read()
-        convert = body.decode('utf-8')
-        print('Body response:\n\t- type: {}'.format(type(body)))
-        print('\t- content: {}\n\t- utf8 content: {}'.
-              format(body, convert, end=''))
-
-    if __name__ == '__main__':
-        status()
+        print("Body response:")
+        ty = response.read()
+        print("\t- type: {}".format(type(response.read())))
+        print("\t- content: {}".format(ty))
+        print("\t- utf8 content: {}".format(ty.decode('utf-8')))
