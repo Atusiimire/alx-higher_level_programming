7c8f40bf23e88ef746830e360a5ff7c60ecef889
Atusiimire <mutatinamoureen@gmail.com>
2022-12-16 14:30:17 -0800

    first commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,9 @@
 if __name__ == "__main__":
     import urllib.request
 
-    with urlopen('https://alx-intranet.hbtn.io/status') as response:
-        print("Body response:")
-        ty = response.read()
-        print("\t- type: {}".format(type(response.read())))
-        print("\t- content: {}".format(ty))
-        print("\t- utf8 content: {}".format(ty.decode('utf-8')))
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    print("Body response:")
+    ty = response.read()
+    print("\t- type: {}".format(type(response.read())))
+    print("\t- content: {}".format(ty))
+    print("\t- utf8 content: {}".format(ty.decode('utf-8')))
