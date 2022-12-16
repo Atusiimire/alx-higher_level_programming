40c3d12392fd7776171d367f05077948065bc8cc
Atusiimire <mutatinamoureen@gmail.com>
2022-12-16 14:14:17 -0800

    first commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""
-fetch https://alx-intranet.hbtn.io/status; display response
-"""
-import urllib.request
+"""fetch url with urlib"""
+from urllib.request import urlopen
 
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+def status():
+    """fetch url body"""
+    with urlopen('https://alx-intranet.hbtn.io/status') as response:
+        body = response.read()
+        convert = body.decode('utf-8')
+        print('Body response:\n\t- type: {}'.format(type(body)))
+        print('\t- content: {}\n\t- utf8 content: {}'.
+              format(body, convert, end=''))
+
+    if __name__ == '__main__':
+        status()
